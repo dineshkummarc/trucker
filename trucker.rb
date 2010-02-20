18361145c93e3876e152fe49a8f51e9eaa06d9cf
John Nunemaker <nunemaker@gmail.com>
2010-02-20 03:03:49 -0500

    Moved to inline files.

--- trucker.rb
+++ trucker.rb
@@ -54,3 +54,73 @@ delete '/p/:id' do
   project.destroy
   {:remove => ["#project_#{project.id}"]}.to_json
 end
+
+__END__
+
+@@ layout
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Trucker</title>
+  <link rel="stylesheet" href="/css/common.css" type="text/css" media="screen" charset="utf-8" />
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="/js/sammy/sammy.js"></script>
+  <script type="text/javascript" src="/js/jquery.form.js"></script>
+  <script type="text/javascript" src="/js/layout.js"></script>
+  <script type="text/javascript" src="/js/application.js"></script>
+</head>
+<body>
+
+  <div id="wrapper">
+    <%= yield %>
+  </div>
+</body>
+</html>
+
+@@ _show
+<h2><a href="#/">Projects</a> &raquo; <%= project.title %></h2>
+
+<form action="/p/<%= project.id %>/s" method="post" class="creator">
+  <p>
+    <input type="text" name="story[body]" value="" id="story_body" size="47" />
+    <input type="submit" name="submit" value="Add" />
+  </p>
+</form>
+
+<ul id="stories" class="list">
+  <% project.stories.each do |story| %>
+    <%= partial('story', :story => story) %>
+  <% end %>
+</ul>
+
+@@ _story
+<li id="story_<%= story.id %>">
+  <%= story.body %>
+</li>
+
+@@ _project
+<li id="project_<%= project.id %>">
+  <a href="/p/<%= project.id %>" class="delete">Delete</a>
+  <a href="#/p/<%= project.id %>"><%= project.title %></a>
+</li>
+
+@@ _projects
+<form action="/p" method="post" class="creator">
+  <p>
+    <input type="text" name="project[title]" value="" id="project_title" size="30" />
+    <input type="submit" name="submit" value="Create Project" />
+  </p>
+</form>
+
+<ul id="projects" class="list">
+  <% projects.each do |project| %>
+    <%= partial('project', :project => project) %>
+  <% end %>
+</ul>
+
+@@ index
+<div id="content"></div>
